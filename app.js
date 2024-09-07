c013e78dd900944fd43d312d62c3695ca9583545
Ranjeet kumar <rcsharma7371@gmail.com>
2024-09-07 14:47:37 +0530

    Inital commit

---
+++ app.js
@@ -0,0 +1,73 @@
+let digit = document.querySelector(".digit");
+let displayValue = document.querySelector("#display");
+
+let calculateData = "";
+let initialState = false;
+let ttlValue = 0;
+digit.addEventListener("click", (event) => {
+    // console.dir(event.target);
+    if ((event.target.className ) == "inpData" || event.target.classList[1] =="inpData") {
+        if(!initialState){
+            displayValue.innerText = "";
+            initialState = true;
+        }
+        if(ttlValue!=0){
+                calculateData = ttlValue;
+                ttlValue=0;
+                displayValue.innerText = calculateData;
+            }
+        // displayValue.innerText="";
+        let inpValue = event.target.name;
+        // console.dir(event.target.name);
+        // if(inpValue==/[0-9]/||inpValue==/[+-*/%.]/){
+            
+        // }
+        displayValue.innerText +=inpValue;
+        calculateData +=inpValue;
+        
+    }
+});
+
+console.log(calculateData);
+
+let equalTo = document.querySelector(".equal");
+equalTo.addEventListener("click", (event) => {
+    let finalValue = eval(calculateData);
+    // displayValue.value = 0;
+    // console.log(calculateData);
+    // console.log(finalValue);
+    displayValue.innerText = finalValue;
+    ttlValue = finalValue;
+    // console.log(displayValue.innerText);
+    calculateData="";
+    initialState=false;
+});
+
+let ac = document.querySelector("#AC");
+ac.addEventListener("click",(event)=>{
+    displayValue.innerText=0;
+    calculateData="";
+    initialState=false;
+    ttlValue=0;
+})
+
+let del = document.querySelector("#DEL");
+del.addEventListener("click",(event)=>{
+
+    updateDelete(calculateData);
+    if(calculateData==undefined){
+        displayValue.innerText = 0;
+
+    }
+    
+})
+function updateDelete(str){
+    str = str.slice(0,-1);
+    calculateData=str;
+    if(calculateData==""){
+        displayValue.innerText = 0;
+
+    }
+    // console.log(str)
+    displayValue.innerText = str;
+}
