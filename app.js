e57318879e73414ee67db0d52de93dd687102857
Ranjeet kumar <rcsharma7371@gmail.com>
2024-09-15 00:23:20 +0530

    Adding media query and features to use keyboard to enter value

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// Code to track mouse and its click
+
 let digit = document.querySelector(".digit");
 let displayValue = document.querySelector("#display");
 
-let calculateData = "";
+let data = "";
 let initialState = false;
-let ttlValue = 0;
+let prevAns = 0;
 digit.addEventListener("click", (event) => {
     // console.dir(event.target);
     if (
@@ -14,50 +16,103 @@ digit.addEventListener("click", (event) => {
             displayValue.innerText = "";
             initialState = true;
         }
-        if (ttlValue != 0) {
-            calculateData = ttlValue;
-            ttlValue = 0;
-            displayValue.innerText = calculateData;
+        if (prevAns != 0) {
+            data = prevAns;
+            prevAns=0;
+            displayValue.innerText = data;
         }
         let inpValue = event.target.name;
         // console.dir(event.target.name);
         displayValue.innerText += inpValue;
-        calculateData += inpValue;
+        data += inpValue;
     }
 });
 
-console.log(calculateData);
+console.log(data);
 
 let equalTo = document.querySelector(".equal");
 equalTo.addEventListener("click", (event) => {
-    let finalValue = eval(calculateData);
-    displayValue.innerText = finalValue;
-    ttlValue = finalValue;
-    calculateData = "";
-    initialState = false;
+    calculate(data);
 });
 
 let ac = document.querySelector("#AC");
 ac.addEventListener("click", (event) => {
-    displayValue.innerText = 0;
-    calculateData = "";
-    initialState = false;
-    ttlValue = 0;
+    clearData();
 });
 
 let del = document.querySelector("#DEL");
 del.addEventListener("click", (event) => {
-    updateDelete(calculateData);
-    if (calculateData == undefined) {
-        displayValue.innerText = 0;
-    }
+    updateDelete(data);
 });
-function updateDelete(str) {
-    str = str.slice(0, -1);
-    calculateData = str;
-    if (calculateData == "") {
-        displayValue.innerText = 0;
+
+
+
+
+//Code to track keyboard keys 
+const operators = ["+", "-", "*", "/", "%", "=", "."];
+
+//Event listener to track pressed key
+window.addEventListener("keydown", (event) => {
+  if (operators.includes(event.key) || (event.key >= "0" && event.key <= "9")) {
+    // console.dir(typeof event.key);
+    if (!initialState) {
+      displayValue.innerText = "";
+      initialState = true;
     }
-    // console.log(str)
-    displayValue.innerText = str;
+    if (prevAns != 0) {
+      if (operators.includes(event.key)) {
+        data = prevAns;
+        prevAns=0;
+        displayValue.innerText = data;
+      }
+    }
+    let inpValue = event.key;
+    // console.dir(event.target.name);
+    displayValue.innerText += inpValue;
+    data += inpValue;
+  }
+
+  if (event.key == "Enter") {
+    calculate(data);
+    data = "";
+  }
+  if (event.key == "Escape") {
+    clearData();
+  }
+
+  if (event.key == "Backspace") {
+    updateDelete(data);
+  }
+});
+
+let calculate = (str) => {
+  if (str == "") {
+    displayValue.innerText = 0;
+  } else {
+    let res = eval(str);
+    displayValue.innerText = res;
+    prevAns = res;
+    console.log(prevAns);
+    initialState=false;
+  }
+};
+
+function clearData(){
+    data = "";
+    displayValue.innerText = "0";
+    prevAns=0;
+    initialState = false;
 }
+
+function updateDelete(str) {
+  if (data == "") {
+    displayValue.innerText = "0";
+  } else {
+    str = str.slice(0, -1);
+    data = str;
+    displayValue.innerText = str;
+    prevAns=0;
+  }
+}
+
+
